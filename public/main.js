945d4461da8704fd2f4622afd1cd24cabec28377
Vlad104 <vlad104khk@gmail.com>
2019-03-08 04:16:01 +0300

    Добавли webpack для js. На css пока не работает

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import './styles.css';
 import {createNavbar} from './pages/navbar/navbar.js';
 import {createMenu} from './pages/menu/menu.js';
 import {createScoreBoard} from './pages/scoreboard/scoreboard.js';
@@ -7,30 +8,6 @@ import {createRules} from './pages/rules/rules.js';
 
 import {renderRegistrationPage, renderLoginPage, renderProfilePage} from "./display_pages.js"
 
-const application = document.getElementById('application');
-
-function ajax (callback, method, path, body) {
-	const xhr = new XMLHttpRequest();
-	xhr.open(method, path, true);
-	xhr.withCredentials = true;
-
-	if (body) {
-		xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-	}
-
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState !== 4) {
-			return;
-		}
-		callback(xhr);
-	};
-
-	if (body) {
-		xhr.send(JSON.stringify(body));
-	} else {
-		xhr.send();
-	}
-}
 
 // К-К-К-КОСТЫЛЬ //
 
