62ffd746b4344c11066079bf2d16bc55cb7fb96f
Howle <mdv413@mail.ru>
2019-03-10 03:06:42 +0300

    Сделал шаблонизацию для профиля, входа и регистрации

--- public/routers_functions.js
+++ public/routers_functions.js
@@ -12,6 +12,9 @@ import {renderRegistrationPage} from './pages/registration/registration.js';
 
 import {MenuComponent} from './components/Menu/Menu.js';
 import {RulesComponent} from './components/Rules/Rules.js';
+import {Signin} from './components/Signin/signin.js';
+import {Registration} from './components/Registration/registration.js';
+import {Profile} from './components/Profile/profile.js';
 import {ScoreBoardComponent} from './components/ScoreBoard/ScoreBoard.js';
 import {RENDER_TYPES} from './utils/constants.js';
 
@@ -59,12 +62,111 @@ export function createNavbarRules() {
 	//createRules();	
 }
 export function createNavbarProfile(me) {
-	createNavbar();
-	renderProfilePage(AjaxModule, me);	
+	// createNavbar();
+	// renderProfilePage(AjaxModule, me);
+
+	const signin = new Profile({
+		el: application,
+		type: RENDER_TYPES.TMPL,
+	})
+	// let listDivs = [{}];
+	if (me) {
+        // listDivs = [
+        //     {left: "Никнейм", right: me.nick},
+        //     {left: "Email", right: me.email},
+        //     {left: "Очки", right: me.score},
+        //     {left: "KDA", right: me.kda},
+		// ];
+		signin.data = {
+			nick: me.nick,
+			email: me.email,
+			score: me.score,
+			kda: me.kda
+		};
+    } else {
+		AjaxModule.doGet({
+			callback(xhr) {
+				if (!xhr.responseText) {
+                    console.log('Unauthorized');
+					//alert('Unauthorized');
+					application.innerHTML = '';
+					createNavbarProfile(AjaxModule);
+					return;
+				}
+				console.log("Sent");
+				const user = JSON.parse(xhr.responseText);
+                application.innerHTML = '';
+                createNavbarProfile(user);
+				// renderProfilePage(user);
+			},
+			path: '/me',
+		});
+		console.log("END ajax");
+	}
+	signin.render();
 }
 export function createLoginPage() {
-	renderLoginPage(AjaxModule);
+	// renderLoginPage(AjaxModule);
+
+	const signin = new Signin({
+		el: application,
+		type: RENDER_TYPES.TMPL,
+	})
+	signin.render();
+	let form = document.getElementsByTagName('form')[0];
+	form.addEventListener("submit", function (event) {
+		event.preventDefault();
+		const email = form.elements[ 'email' ].value;
+		const password = form.elements[ 'password' ].value;
+		AjaxModule.doPost({
+			callback() {
+				application.innerHTML = '';
+				createNavbarProfile();
+			},
+			path: '/login',
+			body: {
+				email,
+				password,
+			},
+		});
+	});
+
 }
 export function createRegistrationPage() {
-	renderRegistrationPage(AjaxModule);
+	// renderRegistrationPage(AjaxModule);
+
+	const signin = new Registration({
+		el: application,
+		type: RENDER_TYPES.TMPL,
+	})
+	signin.render();
+	const form = document.getElementsByTagName('form')[0];
+	const footer = document.getElementsByClassName('registration_input_footer_divblock_registration')[0];
+	footer.addEventListener('click', function (event) {
+		event.preventDefault();
+
+        const nick = form.elements[ 'nick' ].value;
+		const email = form.elements[ 'email' ].value;
+		const password = form.elements[ 'password' ].value;
+		const password_repeat = form.elements[ 'password_repeat' ].value;
+
+		if (password !== password_repeat) {
+			alert('Passwords is not equals');
+			return;
+		}
+
+		AjaxModule.doPost({
+			callback() {
+                application.innerHTML = '';
+                createNavbarProfile();
+				// renderProfilePage();
+			},
+			path: '/signup',
+			body: {
+                nick,
+				email,
+				password,
+			},
+		});
+	});
 }
