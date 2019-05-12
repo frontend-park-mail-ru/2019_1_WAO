5ddab97919a3bdae1064de120448a7cf72a4b14f
Vlad104 <vlad104khk@gmail.com>
2019-05-13 00:59:36 +0300

    fix result score view

--- public/config.js
+++ public/config.js
@@ -1,11 +1,7 @@
 const settings = {
   prefix: 'http://',
   chat: {
-<<<<<<< HEAD
     address: '89.208.197.150:8000',
-=======
-    address: '127.0.0.1:3000',
->>>>>>> develop
   },
   backend: {
     address: '89.208.197.150:8000',
